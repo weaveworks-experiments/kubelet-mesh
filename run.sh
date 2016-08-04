e70e2370e7b34f84c06751f4c122eb115d6758c4
Ilya Dmitrichenko <errordeveloper@gmail.com>
2016-08-04 14:44:20 +0100

    Test case: one peer knows the CA and the other does not know it

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
-./kubelet-mesh -hwaddr 6c:40:08:94:9e:01 -mesh 0.0.0.0:6783 -password VerySecure &
+./kubelet-mesh -hwaddr 6c:40:08:94:9e:01 -mesh 0.0.0.0:6783 -password VerySecure -root-ca ca.crt &
 ./kubelet-mesh -hwaddr 6c:40:08:94:9e:02 -mesh 0.0.0.0:6784 -password VerySecure -peer 127.0.0.1:6783
 until killall kubelet-mesh ; do sleep 1 ; done
